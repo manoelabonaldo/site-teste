38216d5c1018d4d01d8c82874edfdd030debe750
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-03-13 21:55:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return "Olá, mundo! Este é meu site. (Manoela Bonaldo)"
+
+@app.route("/sobre")
+def sobre():
+  return "Aqui vai o conteúdo da página sobre"
+
+@app.route("contato")
+def contato():
+  return "Aqui vai o conteúdo da página contato"
