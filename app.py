6bd5c404fe358b5161910b53561b76a42f100029
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-03-13 22:14:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+menu = """
+<a href="/">Página inicial</a>  |  <a href="/sobre"</a>  |  <a href="/contato">Contato</a> 
+<br>
+"""
+
 @app.route("/")
 def hello_world():
   return "Olá, mundo! Este é meu site. (Manoela Bonaldo)"
