4b1d8bf7ab891df107c86a8781dbda3999743ac1
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-03-20 20:45:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,65 @@
 from flask import Flask
+from tchan import ChannelScraper
 
-app = Flask(__name__)
 
+app = Flask(_name_)
+
+
+def ultimas_promocoes():
+  scraper = ChannelScraper()
+  contador = 0
+  resultado = []
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    resultado.append(f"{message.created_at} {texto}")
+    if contador == 10:
+      return resultado
+
+   
 menu = """
-<a href="/">Página inicial</a>  |  <a href="/sobre">sobre</a>  |  <a href="/contato">contato</a> 
+<a href="/">Página inicial</a> | <a href="/promocoes">PROMOÇÕES</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
 """
 
 @app.route("/")
-def hello_world():
-  return "Olá, mundo! Este é meu site. (Manoela Bonaldo)"
+def index():
+  return menu + "Olá, mundo! Esse é meu site. (Vitor Arthur)"
 
 @app.route("/sobre")
 def sobre():
-  return "Aqui vai o conteúdo da página sobre"
+  return menu + "Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return "Aqui vai o conteúdo da página contato"
+  return menu + "Aqui vai o conteúdo da página Contato"
+
+
+@app.route("/promocoes")
+def promocoes():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  for promocao in ultimas_promocoes():
+    conteudo += f"<li>{promocao}</li>"
+  return conteudo + "</ul>"
+
+
+@app.route("/promocoes2")
+def promocoes2():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  scraper = ChannelScraper()
+  contador = 0
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    conteudo += f"<li>{message.created_at} {texto}</li>"
+    if contador == 10:
+      break
+  return conteudo + "</ul>"
