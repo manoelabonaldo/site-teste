5349d0543ae3cace7f8bd2723dc7399794fbba93
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-03-20 21:13:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 from flask import Flask
 from tchan import ChannelScraper
 
+
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
 TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
 app = Flask(_name_)
