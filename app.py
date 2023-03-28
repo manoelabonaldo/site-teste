e183abcdf37df61b8978f394ea578a095ecb1562
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-03-27 22:07:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 import gspread
 import requests
-from flask import Flask
+from flask import Flask, request
 from oauth2client.service_account import ServiceAccountCredentials
 from tchan import ChannelScraper
 
@@ -89,3 +89,13 @@ def dedoduro():
 def dedoduro2():
   sheet.append_row(["Manoela", "Bonaldo", "a partir do Flask"])
   return "Planilha escrita!"
+
+
+@app.route("/telegram-bot")
+def telegram_bot():
+  update = request.json
+  chat_id = update["message"]["chat"]["id"]
+  message = update["message"]["text"]
+  nova_mensagem = {"chat_id": chat_id, "text": message}
+  requests.post(f"https://api.telegram.org./bot{token}/sendMessage", data=nova_mensagem)
+  return "ok"
