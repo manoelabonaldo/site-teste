604986cab8a6b8fc89af3fa739655d3b0e527eda
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-03-30 21:59:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,13 +32,13 @@ def ultimas_promocoes():
 
     
 menu = """
-<a href="/">Página inicial</a> | <a href="/promocoes">PROMOÇÕES</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<a href="/">Página inicial</a> | <a href="/arquivo_listasuja">Arquivo da Lista Suja</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
 """
 
 @app.route("/")
 def index():
-  return menu + "Olá, mundo! Esse é meu site. (Manoela Bonaldo)"
+  return menu + "Olá, este é o site do robô do trabalho escravo."
 
 @app.route("/sobre")
 def sobre():
@@ -49,8 +49,8 @@ def contato():
   return menu + "Aqui vai o conteúdo da página Contato"
 
 
-@app.route("/promocoes")
-def promocoes():
+@app.route("/arquivo_listasuja")
+def arquivo_listasuja():
   conteudo = menu + """
   Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
   <br>
@@ -58,24 +58,6 @@ def promocoes():
   """
   for promocao in ultimas_promocoes():
     conteudo += f"<li>{promocao}</li>"
-  return conteudo + "</ul>"
-
-
-@app.route("/promocoes2")
-def promocoes2():
-  conteudo = menu + """
-  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
-  <br>
-  <ul>
-  """
-  scraper = ChannelScraper()
-  contador = 0
-  for message in scraper.messages("promocoeseachadinhos"):
-    contador += 1
-    texto = message.text.strip().splitlines()[0]
-    conteudo += f"<li>{message.created_at} {texto}</li>"
-    if contador == 10:
-      break
   return conteudo + "</ul>"
 
 @app.route("/dedoduro")
