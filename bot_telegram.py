10684bc3a95d51eb48d48b8193bc6c4dcc283f10
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-04-05 17:49:29 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -9,7 +9,12 @@ from analise_dados import analisa_listasuja
 GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
 TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
-
+with open("credenciais.json", mode="w") as arquivo:
+  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+api = gspread.authorize(conta)
+planilha = api.open_by_key("1NuMnkxDYzf4CtKPzV1jbKDmwWbM5W7Bld3EqgM9IoHs")
+sheet = planilha.worksheet("Página1")
 
 def bot_dotelegram(update): 
    
@@ -100,5 +105,7 @@ def bot_dotelegram(update):
 
    nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
    resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data = nova_mensagem)
+    # Atualiza planilha do sheets com último update processado
+   sheet.append_rows(mensagens)
 
    return "ok"
