b094bbba378383664614fcf62b3f656855b41332
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-04-04 23:11:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,7 @@ def telegram_bot():
   update = request.json
   if request.method == 'POST':
      update = request.get_json()
+  nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
+  resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data = nova_mensagem)
+  
   return "ok"
