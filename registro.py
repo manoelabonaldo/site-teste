0c6ea79c2428289d6cc4bc4a4ed35b816d595433
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-04-04 22:17:54 -0300

    Update registro.py

--- registro.py
+++ registro.py
@@ -21,3 +21,4 @@ conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
 api = gspread.authorize(conta)
 planilha = api.open_by_key("1NuMnkxDYzf4CtKPzV1jbKDmwWbM5W7Bld3EqgM9IoHs")
 sheet = planilha.worksheet("Página1")
+return "Ok"
