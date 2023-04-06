552d98ac67c3ad70fc8d5e7f3e8638030dcbddc2
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-04-06 19:12:37 -0300

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -11,13 +11,14 @@ from analise_dados import analisa_listasuja
 from datetime import datetime
 
 GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+GOOGLE_SHEETS
 TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
 TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
 with open("credenciais.json", mode="w") as arquivo:
   arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
 conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
 api = gspread.authorize(conta)
-planilha = api.open_by_key("1NuMnkxDYzf4CtKPzV1jbKDmwWbM5W7Bld3EqgM9IoHs")
+planilha = api.open_by_key["GOOGLE_SHEETS_KEY"]
 sheet = planilha.worksheet("Página1")
 
 def bot_dotelegram(update): 
