96335b9b92acacbbb70d6166e58b1d3547c48534
manoelabonaldo <114244292+manoelabonaldo@users.noreply.github.com>
2023-04-06 22:28:31 -0300

    Update salva_link.py

--- salva_link.py
+++ salva_link.py
@@ -2,8 +2,15 @@ import beautifulsoup4
 import gspread
 import oauth2client
 import requests
+import datetime
+import pytz
+import gdown
+import os
 
 from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from bs4 import BeautifulSoup
 
 GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
 
@@ -14,34 +21,46 @@ with open("credenciais.json", mode="w") as arquivo:
   arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
 conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
 api = gspread.authorize(conta)
-planilha = api.open_by_key("1ZDyxhXlCtCjMbyKvYmMt_8jAKN5JSoZ7x3MqlnoyzAM")
-sheet = planilha.worksheet("Sheet1")
+planilha = api.open_by_key("1xR0Xy-m_UWpxofHRf66xX2O50keDnAlexIFdQTOBa2Q/edit#gid=0")
+sheet = planilha.worksheet("Página1")
 
 
+def puxa_listasuja(): 
 
-# Definir URL do site a ser verificado
-url = 'https://www.gov.br/trabalho-e-previdencia/pt-br/pt-br/composicao/orgaos-especificos/secretaria-de-trabalho/inspecao/areas-de-atuacao/combate-ao-trabalho-escravo-e-analogo-ao-de-escravo'
+  url = 'https://www.gov.br/trabalho-e-previdencia/pt-br/pt-br/composicao/orgaos-especificos/secretaria-de-trabalho/inspecao/areas-de-atuacao/combate-ao-trabalho-escravo-e-analogo-ao-de-escravo'
+  response = requests.get(url)
+  soup = BeautifulSoup(response.content, "html.parser")
+  links = soup.find_all('a')
 
-# Fazer requisição HTTP para o site e obter seu conteúdo HTML
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
+  xlsx_links = []
+  for link in soup.find_all("a"):
+      href = link.get("href")
+      if href and href.endswith(".xlsx") and "cadastro_de_empregadores" in href:
+          xlsx_links.append(href)
 
-# Analisar o conteúdo HTML com BeautifulSoup e encontrar todos os links
-soup = BeautifulSoup(response.content, 'html.parser')
-links = soup.find_all('a')
-
-xlsx_links = []
-for link in soup.find_all("a"):
-    href = link.get("href")
-    if href and href.endswith(".xlsx") and "cadastro_de_empregadores" in href:
-        xlsx_links.append(href)
-
-if xlsx_links:
-    for link in xlsx_links:
-        print(link)
-else:
-    print("Não foi encontrado nenhum arquivo .xlsx files contendo a expressão 'cadastro_de_empregadores'.")
+  if xlsx_links:
+      for link in xlsx_links:
+          print(link)
+  else:
+      print("Não foi encontrado nenhum arquivo .xlsx files contendo a expressão 'cadastro_de_empregadores'.")
     
-lista_sujaatual = link
-sheet.append_row([lista_sujaatual])
+  lista_sujaatual = link
 
+  range_name = 'A1:B1'
+
+  timezone = 'America/Sao_Paulo'
+  now = datetime.datetime.now(pytz.timezone(timezone)).strftime('%m/%d/%Y %H:%M:%S')
+  service = build('sheets', 'v4', credentials=conta)
+  values = [
+      [link, now]
+  ]
+  request = service.spreadsheets().values().append(
+      spreadsheetId=SPREADSHEET_ID,
+      range=range_name,
+      valueInputOption='USER_ENTERED',
+      insertDataOption='INSERT_ROWS',
+      body={'values': values}
+  )
+  response = request.execute()
+  print('Informações adicionadas com sucesso.')
+  Return "OK"
